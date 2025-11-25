bc3b5fb26bdc5ac1ee0e19196412d2ddfa0dde06
caoasuka <137641403+caoasuka@users.noreply.github.com>
2025-11-25 19:25:47 +0800

    Site updated: YYYY-11-2025年11月25日 19:25:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1764068232254";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1764069947406";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
